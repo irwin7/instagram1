b33828dcd26d18d1209cf42726e69c0723cc8ded
irwin7 <ravshankhan777@icloud.com>
2022-01-06 10:30:53 +0500

    pushed

--- js/date.js
+++ js/date.js
@@ -15,42 +15,54 @@ let acc = [
         posts : [
             {
                 id : 1,
+                date : 'SEPTEMBER 18, 2021',
                 likes : 1453,
                 liked : false,
-                views : 4345,
+                comment : 4345,
                 type : "images",
                 title : "Most expansive pc",
-                content : {
-                    img1 : "media/infinity_setups/post1/242116778_547895796492364_8534420977603997970_n.jpg",
-                    img2 : "media/infinity_setups/post1/242213793_531879544577844_7762584045485569229_n.jpg",
-                    img3 : "media/infinity_setups/post1/242376982_545985013349324_2580880256080923931_n.jpg"
-                }
+                content : [
+                    "media/infinity_setups/post1/242116778_547895796492364_8534420977603997970_n.jpg","media/infinity_setups/post1/242213793_531879544577844_7762584045485569229_n.jpg","media/infinity_setups/post1/242376982_545985013349324_2580880256080923931_n.jpg"
+                ],
+                commAuthor : `Rate this mac setup 1-10! 😍<br>
+                [via @detechtiveofficial]<br><br>
+                ♥️ Follow @infinity.setups for daily content<br>
+                👊 Share with your friends!<br>.<br>.<br>.<br>.<br>.<br>.<br>
+                #geekgamer #gaminghub #corsairgaming #ultimategamingpc #gamernation #gamingbuild #pcgamingsetups #computergaming #gamersofig #desksetups #gamerpc #cleansetup #cleansetups #gamingpcbuild #setupwar #gamercommunity #pcaddicts #officialsetups #pcgamingsetup #pcbuilder #pcsetups #workstation #gamersunite #desksetup #gamingroom #gamingislife #gamergram #dreamsetup`
             },
             {
                 id : 2,
+                date : 'NOVEMBER 26, 2021',
                 likes : 1234,
                 liked : false,
-                views : 5324,
+                comment : 5324,
                 type : "images",
                 title : "Most best mest pc",
-                content : {
-                    img1 : "media/infinity_setups/post2/242008575_380792310270623_4598207805913108702_n.jpg",
-                    img2 : "media/infinity_setups/post2/242149431_3063048710683106_3941813622403777324_n.jpg",
-                    img3 : "media/infinity_setups/post2/242170830_289837492541224_3667126288800243741_n.jpg"
-                }
+                content : [
+                    "media/infinity_setups/post2/242008575_380792310270623_4598207805913108702_n.jpg","media/infinity_setups/post2/242149431_3063048710683106_3941813622403777324_n.jpg","media/infinity_setups/post2/242170830_289837492541224_3667126288800243741_n.jpg"
+                ],
+                commAuthor : `Rate this mac setup 1-10! 😍<br>
+                [via @detechtiveofficial]<br><br>
+                ♥️ Follow @infinity.setups for daily content<br>
+                👊 Share with your friends!<br>.<br>.<br>.<br>.<br>.<br>.<br>
+                #geekgamer #gaminghub #corsairgaming #ultimategamingpc #gamernation #gamingbuild #pcgamingsetups #computergaming #gamersofig #desksetups #gamerpc #cleansetup #cleansetups #gamingpcbuild #setupwar #gamercommunity #pcaddicts #officialsetups #pcgamingsetup #pcbuilder #pcsetups #workstation #gamersunite #desksetup #gamingroom #gamingislife #gamergram #dreamsetup`
             },
             {
                 id : 3,
+                date : 'SEPTEMBER 25, 2021',
                 likes : 4433,
                 liked : true,
-                views : 53444,
+                comment : 53444,
                 type : "images",
                 title : "Most the best pc",
-                content : {
-                    img1 : "media/infinity_setups/post1/242116778_547895796492364_8534420977603997970_n.jpg",
-                    img2 : "media/infinity_setups/post1/242213793_531879544577844_7762584045485569229_n.jpg",
-                    img3 : "media/infinity_setups/post1/242376982_545985013349324_2580880256080923931_n.jpg"
-                }
+                content : [
+                    "media/infinity_setups/post3/241221239_103684292015843_7423118308495894737_n.jpg","media/infinity_setups/post3/241271743_365861811915984_5096489011558940377_n.jpg"
+                ],
+                commAuthor : `Rate this mac setup 1-10! 😍<br>
+                [via @detechtiveofficial]<br><br>
+                ♥️ Follow @infinity.setups for daily content<br>
+                👊 Share with your friends!<br>.<br>.<br>.<br>.<br>.<br>.<br>
+                #geekgamer #gaminghub #corsairgaming #ultimategamingpc #gamernation #gamingbuild #pcgamingsetups #computergaming #gamersofig #desksetups #gamerpc #cleansetup #cleansetups #gamingpcbuild #setupwar #gamercommunity #pcaddicts #officialsetups #pcgamingsetup #pcbuilder #pcsetups #workstation #gamersunite #desksetup #gamingroom #gamingislife #gamergram #dreamsetup`
             },
         ]
     }
--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
 let elNavigItems = document.querySelectorAll('.posts__item');
+let elPostList = document.querySelector('.posts__list');
+let postTemp = document.querySelector('#postTemplate').content;
+let elModalTemp = document.querySelector('#modalPost').content;
+let elPostFrag = document.createDocumentFragment();
+let elLikeBtn = document.querySelector('.btn-like');
+let elPost = document.querySelectorAll('.post');
+let elPosts = document.querySelector('.posts');
+let elPostInner = document.querySelectorAll('.post__inner');
+let elFullPost = document.querySelector('.fullpost');
 
+
+
+
+
+showPost();
 
 for(let item of elNavigItems){
     item.addEventListener('click',()=>{
@@ -8,4 +22,77 @@ for(let item of elNavigItems){
         }
         item.classList.add('posts__item--active');
     })
+}
+
+function showPost(){
+    for(let item of acc[0].posts){
+        let postClone = postTemp.cloneNode(true);
+        postClone.querySelector('.post').id = `${item.id - 1}`;
+        postClone.querySelector('.post__img').src = `${item.content[0]}`;
+        postClone.querySelector('.post__likes-count').textContent = `${item.likes}`;
+        postClone.querySelector('.post__comm-count').textContent = `${item.comment}`;
+        postClone.querySelector('.post__inner').addEventListener('click',()=>{
+            showModal(item.id - 1);
+        })
+        elPostFrag.appendChild(postClone);
+        elPostList.appendChild(elPostFrag);
+    }
+}
+
+function showModal(id){
+    let clone = elModalTemp.cloneNode(true);
+    let cont = acc[0].posts[id];
+    for(let item of cont.content){
+        clone.querySelector('.fullpost__content').innerHTML = `<img class="fullpost__img" src="${item}" alt="">`;
+    }
+    clone.querySelector('.info__ava').src = acc[0].ava;
+    clone.querySelector('.info__user').textContent = acc[0].userName;
+    clone.querySelector('.comms__author-usern').textContent = acc[0].userName;
+    clone.querySelector('.comms__author-ava').src = acc[0].ava;
+    clone.querySelector('.comms__author-comment').innerHTML = cont.commAuthor;
+    
+    follow(clone);
+    likedCheck(clone);
+    function likedCheck(item){
+        if(cont.liked === true){
+            item.querySelector('.btn-like').innerHTML = `<i class='bx bxs-heart bxs-heart--active' ></i>`;
+        }else{
+            item.querySelector('.btn-like').innerHTML = `<i class='bx bx-heart' ></i>`;
+        }
+    }
+    function follow(temp){
+        if(acc[0].isFollowed === true){
+            temp.querySelector('.info__is-followed').textContent = 'Following';
+        }else{
+            temp.querySelector('.info__is-followed').textContent = 'Follow';
+        }
+    }
+    clone.querySelector('.info__is-followed').addEventListener('click',()=>{
+        if(acc[0].isFollowed === true){
+            acc[0].isFollowed = false;
+            follow(document);
+        }else{
+            acc[0].isFollowed = true;
+            follow(document);
+        }
+    })
+    clone.querySelector('.btn-like').addEventListener('click',()=>{
+        if(cont.liked === true){
+            cont.liked = false;
+            cont.likes--;
+            likedCheck(document);
+            document.querySelector('.info__likes-count').textContent = cont.likes;
+        }else{
+            cont.liked = true;
+            cont.likes++;
+            likedCheck(document);
+            document.querySelector('.info__likes-count').textContent = cont.likes;
+        }
+    })
+    clone.querySelector('.info__likes-count').textContent = cont.likes;
+    clone.querySelector('.fullpost__exit').addEventListener('click',()=>{
+        elPosts.removeChild(elPosts.childNodes[elPosts.childNodes.length -2])
+    })
+    elPosts.appendChild(clone)
+    console.log(elPosts);
 }
